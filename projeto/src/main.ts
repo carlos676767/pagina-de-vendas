700e5150e6355ca5e1b8b7bc7b8edbc491ab1d9d
carlos676767 <carlitobr730@gmail.com>
2024-02-12 10:55:27 -0800

    sweetalert colocado.

--- projeto/src/main.ts
+++ projeto/src/main.ts
@@ -1,12 +1,23 @@
+import Swal from 'sweetalert2';
 const input = document.getElementById("input") as HTMLInputElement;
+
+const mensagemDeEnvioEmail = () => {
+    Swal.fire({
+      title: "Email Enviado",
+      text: "Seu email foi enviado com sucesso!",
+      icon: "success",
+    });
+  };
 
 const verificarInputValor = () => {
   let mostrar = document.querySelector(".mostrar") as HTMLParagraphElement;
   let obterDadosInput = input.value;
-  if (obterDadosInput === "") {;
-    mostrar.classList.add("textoBlack")
+  if (obterDadosInput === "") {
+    mostrar.classList.add("textoBlack");
     mostrar.innerHTML = "Digite um email valido.";
-    input.classList.add("inputVermelho")
+    input.classList.add("inputVermelho");
+  }else{
+    mensagemDeEnvioEmail()
   }
 };
 
