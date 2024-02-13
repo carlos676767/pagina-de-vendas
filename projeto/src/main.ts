6b34f2d7643852db999754256d41aef1f4d9787f
carlos676767 <carlitobr730@gmail.com>
2024-02-13 12:11:07 -0800

    faendo o darkmode

--- projeto/src/main.ts
+++ projeto/src/main.ts
@@ -30,3 +30,32 @@ const botaoVerificar = document.getElementById("botaoVerificar");
 botaoVerificar?.addEventListener("click", () => {
   verificarInputValor();
 });
+
+
+const darkMode = () => {
+  const armezenarBody = document.body
+  const bodyBlack = () => {
+    armezenarBody.style.backgroundColor = `black`
+  }
+  const darkModeButton = document.getElementById("darkModeToggle") as HTMLInputElement;
+  darkModeButton.addEventListener("change", () => {
+    if (darkModeButton.checked) {
+      bodyBlack()
+      localStorage.setItem("black", "black")
+    }
+  });
+
+  const salvarDarkMode = () => {
+    const recuperarValor = localStorage.getItem("black")
+    console.log(recuperarValor);
+    if (recuperarValor === `black`) {
+      bodyBlack()
+    }
+  }
+
+  salvarDarkMode()
+};
+
+darkMode();
+
+
